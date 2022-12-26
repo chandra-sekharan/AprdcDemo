7e12392649384f058b00f7a560f4e3c3c12b27b8
chandra-sekharan <chandrsekharan.cs@gmail.com>
2022-12-26 15:37:59 +0530

    second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,9 @@ import ncc from './images/ncc.jpg'
 import sports from './images/sports.jpg'
 import arrowleft from './images/arrowleft.png'
 import nss from './images/ncc.jpg'
+import physics from './images/physics.PNG'
+import chemistry from './images/chemistry.jpeg';
+
 const Home = ()=>{
  
     const Moments = [
@@ -24,11 +27,27 @@ const Home = ()=>{
         {
             id:3,
             src:nss,
-            title:"National ServiceScheme",
+            title:"National Service Scheme",
             desc:"Lorium ipsem is a dummy text used for random purpose it is used by developers for dummy content",
         },
     ]
+ const lab = [
+    {
+        id:1,
+        src:physics,
+        title:"Physics Laboratory",
+        desc:"Lorium ipsem is a dummy text used for random purpose it is used by developers for dummy content",
+    },
+    {
+        id:2,
+        src:chemistry,
+        title:"Chemistry Laboratory",
+        desc:"Lorium ipsem is a dummy text used for random purpose it is used by developers for dummy content",
+    },
+ ]
 
+ 
+ 
     return(
         <>
         <div className="Home">
@@ -37,7 +56,7 @@ const Home = ()=>{
         </div>
         <div className="homesection">
         <div className="events">
-            <h1>Moments of APRDC</h1>
+            <h1>HAPPENINGS IN APRDC</h1>
             {Moments.map(data =>
             <div className="event">
               <div className="image">
@@ -50,6 +69,19 @@ const Home = ()=>{
             )}
             <footer></footer>
             </div> 
+            <div className="events">
+                <h1>HANDS-ON LAB</h1>
+                {lab.map(data=>
+                <div className="event">
+                   <div className="image">
+                    <img src={data.src} alt="" />
+                   </div>
+                  <h2>{data.title}</h2>
+                  <h6>{data.desc}</h6>
+                  <img src={arrowleft} id="more" /><span>Visit For More</span>
+                </div>
+               )}
+            </div>
             </div>
         </>
     );
