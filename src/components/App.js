fe259b1eb6052dffa14118be9e71672854473029
chandra-sekharan <chandrsekharan.cs@gmail.com>
2023-01-15 20:25:53 +0530

    APRDC

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
-import React , {useState} from "react";
+import React , {useState , useEffect} from "react";
 import Router from "./Router";
 import Loader from "./Loader";
 
 const App = () =>{
 
-  const [loading , setLoading] = useState(true);
+  const [loading , setLoading] = useState(false);
   
+ useEffect(()=>{
+
+   setLoading(true);   
+
   setTimeout(()=>{
     setLoading(false)
-  },1600)
+  },2000)
+ },[])
+  
   
 
   return(
